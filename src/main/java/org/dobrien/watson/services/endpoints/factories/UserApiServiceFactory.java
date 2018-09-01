e4c2744c3d4568c47c59a2728312b39de39ac1b4
Dermot O'Brien <dermots.mail@gmail.com>
2018-09-01 15:08:35 +1200

    Regenerate api.

--- src/main/java/org/dobrien/watson/services/endpoints/factories/UserApiServiceFactory.java
+++ src/main/java/org/dobrien/watson/services/endpoints/factories/UserApiServiceFactory.java
@@ -3,7 +3,7 @@ package org.dobrien.watson.services.endpoints.factories;
 import org.dobrien.watson.services.endpoints.UserApiService;
 import org.dobrien.watson.services.endpoints.impl.UserApiServiceImpl;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2018-08-13T16:54:53.643+12:00")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2018-09-01T15:02:02.417+12:00")
 public class UserApiServiceFactory {
     private final static UserApiService service = new UserApiServiceImpl();
 
--- src/main/java/org/dobrien/watson/services/endpoints/impl/UserApiServiceImpl.java
+++ src/main/java/org/dobrien/watson/services/endpoints/impl/UserApiServiceImpl.java
@@ -16,7 +16,7 @@ import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import javax.validation.constraints.*;
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2018-08-13T16:54:53.643+12:00")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2018-09-01T15:02:02.417+12:00")
 public class UserApiServiceImpl extends UserApiService {
     @Override
     public Response getUser(SecurityContext securityContext) throws NotFoundException {
